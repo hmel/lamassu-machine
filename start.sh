f70aa837c0a4300d8db26bed46ade4c5d70f5e00
Nikolay Mitev <face@hmel.org>
2016-12-16 14:16:09 +0200

    Working barcode scanner with zbar library. Fix cancel scan bug

---
+++ start.sh
@@ -0,0 +1,1 @@
+node --inspect --debug-brk bin/lamassu-machine --mockBTC 16Yfrfu7RVDuq45kdGCopbECu8W6n7wVWi --mockBv /dev/pts/10 --mockTrader --mockBillDispenser --mockCam --mockApi
---
+++ test/test_bitcoin_address.js
@@ -0,0 +1,3 @@
+var bitcoinAddressValidator = require('bitcoin-address')
+
+console.log(bitcoinAddressValidator.validate("1DQKHXJAb5YtRFQET5ZX6PLkQgoJQD3Cdc"));
